79ae564982d7dd98b8b605ffd904cc50585b9160
TahaBakk <tahabakk2@gmail.com>
2017-04-06 19:26:36 +0200

    asdasd

--- src/DAO.java
+++ src/DAO.java
@@ -1,3 +1,7 @@
+import org.basex.api.client.ClientSession;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -25,21 +29,46 @@ public class DAO {
 
 
 
-    public static void coneccion(){
+    public static void coneccion() throws IOException {
 
         ClientSession session = new ClientSession("localhost", 1984, "admin", "admin");
 
     }
 
-    public static void createColecciones(){
-
+    public static void createColecciones(String username, String pass, String ip) throws IOException {
+        BaseXClient session = new BaseXClient(ip, 1984, username, pass);
+        session.execute("create db TahaBakk");
+        System.out.println(session.info());
     }
 
-    public static void createResursos(){
 
+    public static void createResursos(String username, String pass, String ip) throws IOException {
+        BaseXClient session = new BaseXClient(ip, 1984, username, pass);
+        ByteArrayInputStream bais = new ByteArrayInputStream("<x>Hello im taha !^_^!</x>".getBytes());
+        session.add("TahaBakk/UF3-ExamenF-Plantes.xml", bais);
+        System.out.println(session.info());
     }
 
-    public static void consultas(){
+    public static void consultas(String username, String pass, String ruta, String bbdd, String consulta, String ip, String tipo){
+
+        /*try (ClientSession session = new ClientSession(ip, 1984, username, pass)) {//IP, PUERTO, USERNAME, PASSWORD
+            session.execute(new CreateDB(bbdd, ruta));//BBDD que se creara, y ruta del fitxero
+            try (ClientQuery query = session.query(consulta)) {//consulta
+                if (tipo == "consulta"){
+                    System.out.println(query.execute());//imprimir resultado
+                }else if (tipo.equalsIgnoreCase("parserXML")){
+                    parserXML(query.execute(),"tercera");
+                }else if (tipo.equalsIgnoreCase("segunda")){
+                    parserXML(query.execute(),"segunda");
+                }
+            } catch (SAXException | ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+            session.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
 
     }
 
